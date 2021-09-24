ad4233187244429de808cad95db263fc518e736c
Shruthic27 <76080986+Shruthic27@users.noreply.github.com>
2021-09-23 19:12:28 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,5 +11,37 @@ function gotResults(error,results){
        console.error(error) 
     } else {
         console.log(results)
+        rnr = Math.floor(Math.random() * 256)
+        rnb = Math.floor(Math.random() * 256)
+        rng = Math.floor(Math.random() * 256)
+        document.getElementById("S").innerHTML = results[0].label;
+        document.getElementById("A").innerHTML = (results[0].confidence * 100).toFixed(2) + " %"
+        document.getElementById("S").style.color = "rgb("
+            + rnr + "," + rng + "," + rnb +
+            ")";
+        document.getElementById("A").style.color = "rgb("
+            + rnr + "," + rng + "," + rnb +
+            ")";
+
+        A1 = document.getElementById('I1');
+        
+
+        if (results[0].label == "Bark") {
+            A1.src = 'dog.png';
+            
+        } else if (results[0].label == "Meow") {
+            A1.src = 'cat.png';
+            
+        } else if (results[0].label == "Baa") {
+            A1.src = 'sheep.png';
+            
+        } else if (results[0].label == "roar") {
+            A1.src = 'lion.png'; 
+        } else{
+            A1.src = 'sound.png';
+                }
+            
+           
+        
     }
 }
